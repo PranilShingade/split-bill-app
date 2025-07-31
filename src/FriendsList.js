609d5c136afec4dd75d647e4dd90c964de418a22
PranilShingade <pratik.pranilshingade@gmail.com>
2025-07-31 15:33:28 +0530

    made a eat-n-slip app initial commit

---
+++ src/FriendsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Friend from "./Friend";
+
+export default function FriendsList({friends, onSelection, selectedFriend}) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          key={friend.id}
+          friend={friend}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
+
